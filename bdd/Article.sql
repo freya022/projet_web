902e29b4f931f407302ee3dc119bc0c51803cea2
freya022 <41875020+freya022@users.noreply.github.com>
2021-12-10 14:12:19 +0100

    Remplacer int par serial dans les clés primaires

--- bdd/Article.sql
+++ bdd/Article.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS Article;
 
 CREATE TABLE Article
 (
-    idArticle   int primary key,
+    idArticle   serial primary key,
     nomArticle  text           not null,
     description text           not null,
     prix        numeric(10, 2) not null
--- bdd/Reparation.sql
+++ bdd/Reparation.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS Reparation;
 
 CREATE TABLE Reparation
 (
-    idReparation      int primary key,
+    idReparation      serial primary key,
     articleReparation text references Article (nomArticle),
     magasinReparation text references magasin (nomMagasin),
     prix              numeric(10, 2) not null,
