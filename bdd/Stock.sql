124319d7f10994fd2b2c911a2b0edab798b24903
freya022 <41875020+freya022@users.noreply.github.com>
2021-12-10 11:53:15 +0100

    Suppression des attributs existants

--- bdd/Stock.sql
+++ bdd/Stock.sql
@@ -3,10 +3,7 @@ DROP TABLE IF EXISTS StockArticle CASCADE;
 CREATE TABLE StockArticle
 (
     idArticle     serial primary key,
-    nom           text           not null,
-    description   text,
     prix          numeric(10, 2) not null,
-    typeVelo      text,
     quantiteDispo int            not null,
     quantiteMin   int
 );
