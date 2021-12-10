040e4726553e0110ba44b01a9eb34f8096794168
YorannD <95905366+YorannD@users.noreply.github.com>
2021-12-10 11:26:11 +0100

    Update Stock.sql

--- bdd/Stock.sql
+++ bdd/Stock.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS StockArticle CASCADE;
 
 CREATE TABLE StockArticle
 (
-    idArticle     int primary key AUTO_INCREMENT,
+    idArticle     serial primary key,
     nom           text           not null,
     description   text,
     prix          numeric(10, 2) not null,
