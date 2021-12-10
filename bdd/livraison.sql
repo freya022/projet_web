f458174d54f9ad4fb99526aa1d8ce331b0e0064d
antoineC <78803429+antoine59164@users.noreply.github.com>
2021-12-10 12:16:53 +0100

    Update livraison.sql

--- bdd/livraison.sql
+++ bdd/livraison.sql
@@ -2,6 +2,7 @@ CREATE TABLE Livraison
 (
     idLivraison serial primary key ,
     dateLivraisonPrevu TIMESTAMP not null ,
-    dateArrive TIMESTAMP not null,
+    dateArrive TIMESTAMP not null
+   
 
 );
