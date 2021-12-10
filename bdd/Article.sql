78a4bed1a0f6983eb72f3fb37b01d76b9695f891
freya022 <41875020+freya022@users.noreply.github.com>
2021-12-10 11:54:25 +0100

    Reformatage

--- bdd/Article.sql
+++ bdd/Article.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS Article;
 
 CREATE TABLE Article
 (
-    idArticle int primary key,
-    nom_article text not null,
-    description text not null,
-    prix numeric(10, 2) not null
+    idArticle   int primary key,
+    nomArticle  text           not null,
+    description text           not null,
+    prix        numeric(10, 2) not null
 );
