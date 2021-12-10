e38ee65380592f6f4b34a489fa2cd9630dd9b060
freya022 <41875020+freya022@users.noreply.github.com>
2021-12-10 14:52:44 +0100

    Ne pas faire les requêtes SQL en case-sensitive

--- BDD.js
+++ BDD.js
@@ -4,7 +4,8 @@ const {Sequelize, DataTypes} = require("sequelize");
 const sequelize = new Sequelize(config.databaseName, config.user, config.password, {
     port: config.port,
     host: 'localhost',
-    dialect: "postgres"
+    dialect: "postgres",
+    quoteIdentifiers: false
 });
 
 module.exports = {
