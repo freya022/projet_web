9e663018120fd8ca8c91eda1868055780cefc4d9
YorannD <yorann.degardin@uphf.fr>
2021-12-10 10:45:16 +0100

    Creation de la bdd du Stock des articles.

---
+++ bdd/Stock.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS StockArticle CASCADE;
+
+CREATE TABLE StockArticle
+(
+    idArticle     int primary key,
+    nom           text           not null,
+    description   text,
+    prix          numeric(10, 2) not null,
+    typeVelo      text,
+    quantiteDispo int            not null,
+    quantiteMin   int
+);
+
