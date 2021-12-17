e2804574ddc4524558b45fc2a41cbaabdb1b59ec
YorannD <yorann.degardin@uphf.fr>
2021-12-17 08:52:44 +0100

    BDD Article Piece

--- ArticlePiece.js
+++ ArticlePiece.js
@@ -4,11 +4,6 @@ const article = sequelize.define(
     "ArticlePiece",
     {
         idArticle: {type: DataTypes.INTEGER, primaryKey: true},
-        nomArticle: DataTypes.TEXT,
-        categorie: DataTypes.TEXT,
-        description: DataTypes.TEXT,
-        lienImage: DataTypes.TEXT,
-        prix: DataTypes.FLOAT,
     },
     {
         tableName: "article",
