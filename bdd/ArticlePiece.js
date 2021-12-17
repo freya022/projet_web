17e7574722e4587fe836b17d402c6962e4a2ed5c
freya022 <41875020+freya022@users.noreply.github.com>
2021-12-17 10:09:21 +0100

    Retirer les attributs communs

--- bdd/ArticlePiece.js
+++ bdd/ArticlePiece.js
@@ -3,15 +3,10 @@ const {sequelize, DataTypes} = require('./../BDD.js')
 const article = sequelize.define(
     "ArticlePiece",
     {
-        idArticle: {type: DataTypes.INTEGER, primaryKey: true},
-        nomArticle: DataTypes.TEXT,
-        categorie: DataTypes.TEXT,
-        description: DataTypes.TEXT,
-        lienImage: DataTypes.TEXT,
-        prix: DataTypes.FLOAT,
+        idArticle: {type: DataTypes.INTEGER},
     },
     {
-        tableName: "article",
+        tableName: "articlepiece",
         timestamps: false
     }
 );
--- bdd/ArticlePiece.sql
+++ bdd/ArticlePiece.sql
@@ -1,9 +1,4 @@
 CREATE TABLE ArticlePiece
 (
-    idArticle   serial primary key,
-    nomArticle  text           not null,
-    categorie   text           not null,
-    description text           not null,
-    lienImage   text           not null,
-    prix        numeric(10, 2) not null
+    idArticle serial not null references Article
 );
